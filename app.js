e149af0b87b685d80de247840380a58b4aa25010
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-23 13:43:49 +0530

    Express task 2 Done

--- app.js
+++ app.js
@@ -24,40 +24,106 @@ const contactDetails = {
 app.get('/contact', (req,res)=>{
     res.send(contactDetails);
 })
-let items = [
-    {
-      "name": "Hydrating Serum",
-      "price": "$25",
-      "description": "A lightweight serum that deeply hydrates and plumps the skin."
-    },
-    {
-      "name": "Vitamin C Cream",
-      "price": "$30",
-      "description": "Brightens skin tone and reduces the appearance of dark spots."
-    }
-  ]
-app.get('/products', (req,res)=>{
+// let items = [
+//     {
+//       "name": "Hydrating Serum",
+//       "price": "$25",
+//       "description": "A lightweight serum that deeply hydrates and plumps the skin."
+//     },
+//     {
+//       "name": "Vitamin C Cream",
+//       "price": "$30",
+//       "description": "Brightens skin tone and reduces the appearance of dark spots."
+//     }
+//   ]
+// app.get('/products', (req,res)=>{
     
-    res.send(items);    
+//     res.send(items);    
+// })
+
+// app.post('/products', (req,res)=>{
+//     console.log(req.body);
+//     const {name, price, description} = req.body;
+ 
+//     if(!name ||!price ||!description){
+//         return res.status(400).json({
+//             "error": "All fields are required"
+//         });
+//     }
+//     const newItem = {
+//         name,
+//         price,
+//         description
+//     }
+//     items.push(newItem);
+
+//     res.status(201).json(newItem);
+// })
+
+let orders = [
+  { id: 1, product: 'Anti-Aging Serum', quantity: 2 },
+  { id: 2, product: 'Vitamin C Moisturizer', quantity: 1 },
+  { id: 3, product: 'Hyaluronic Acid', quantity: 3 }
+]
+
+app.get('/orders/:orderID', (req,res)=>{
+    const orderID = req.params.orderID;
+    const order = orders.find(order => order.id === parseInt(orderID));
+
+    if(!order){
+        res.status(404).json({"error": "Order not found"});
+    }
+
+    res.status(200).json(order);
 })
 
-app.post('/products', (req,res)=>{
-    console.log(req.body);
-    const {name, price, description} = req.body;
+let products = [
+  { id: 11, name: "Retinol Serum", price: 1200, availableQty: 50 },
+  { id: 12, name: "Niacinamide Solution", price: 800, availableQty: 30 },
+  { id: 14, name: "Peptide Moisturizer", price: 1500, availableQty: 100 },
+  { id: 15, name: "Glycolic Acid Toner", price: 900, availableQty: 20 }
+]
+
+app.get('/products', (req,res)=>{
+    const {name, maxPrice} = req.body;
+    if(!name && !maxPrice ){
+        return res.status(200).json(products);
+    }
+    let product;
+    if(name && !maxPrice){
+        product = products.filter(product => product.name === name);
+        return res.status(200).json(product);
+    }
+    if(!name && maxPrice){
+        product = products.filter(product => product.price <= maxPrice);
+        return res.status(200).json(product);
+    }
+    product = products.filter(product => product.name === name && product.price <= maxPrice);
+    return res.status(200).json(product);
+})
+
+let shoppingCart = []
+app.get('/cart', (req,res)=>{
+    res.status(200).json(shoppingCart);
+})
+
+app.post('/cart', (req,res)=>{
+    const {id, name, price} = req.body;
  
-    if(!name ||!price ||!description){
+    if(!name ||!price ||!id){
         return res.status(400).json({
             "error": "All fields are required"
         });
     }
     const newItem = {
+        id,
         name,
-        price,
-        description
+        price
     }
-    items.push(newItem);
+    shoppingCart.push(newItem);
 
     res.status(201).json(newItem);
+
 })
 
 app.get('*', (req, res)=>{
